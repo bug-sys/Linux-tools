31823b7fb8074bbff1114f0009f8e053bf10ecb4
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-06-07 12:48:19 +0700

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+set -e
 
 # Fungsi untuk menampilkan daftar disk yang tersedia
 daftar_disk() {
-    lsblk -d -o NAME,SIZE,TYPE | awk '/disk/ {print}'
+    local disks=$(lsblk -d -o NAME,SIZE,TYPE | awk '/disk/ {print}')
+    local i=1
+    echo "$disks" | while read -r line; do
+        echo "$i. $line"
+        ((i++))
+    done
 }
 
 # Fungsi untuk memformat partisi
@@ -13,20 +19,18 @@ format_partisi() {
     local size_root=$4
     local label_boot=$5
     local label_root=$6
-
     echo "Memulai pembuatan MBR dan partisi pada $dev"
     parted -s "$dev" mklabel msdos
     parted -s "$dev" mkpart primary fat32 "$start_boot" "$size_boot"
     parted -s "$dev" mkpart primary ext4 "$size_boot" "$size_root"
-
+    sync
     echo "Memformat partisi BOOT..."
     mkfs.vfat -n "$label_boot" "${dev}1"
-    echo "Proses format partisi BOOT selesai."
-
+    echo "selesai."
     echo "Memformat partisi ROOT..."
     mke2fs -F -q -t ext4 -L "$label_root" -m 0 "${dev}2"
     e2fsck -n "${dev}2"
-    echo "Proses format partisi ROOT selesai."
+    echo "selesai."
 }
 
 # Fungsi untuk mengkloning sistem ke partisi
@@ -34,49 +38,38 @@ klon_sistem() {
     local dev=$1
     local label_boot=$2
     local label_root=$3
-
     PART_BOOT="${dev}1"
     PART_ROOT="${dev}2"
     DIR_CLONE="/ddbr/clone"
-
-    rm -rf "$DIR_CLONE" || true
-    mkdir -p "$DIR_CLONE"
-
-    if grep -q "$PART_BOOT" /proc/mounts ; then
+    if [ -d $DIR_CLONE ] ; then
+        rm -rf $DIR_CLONE
+    fi
+    mkdir -p $DIR_CLONE
+    if grep -q $PART_BOOT /proc/mounts ; then
         echo "Melepaskan partisi BOOT."
-        umount "$PART_BOOT" || true
+        umount -f $PART_BOOT || true
     fi
-
     echo "Menyalin BOOT..."
-    mount "$PART_BOOT" "$DIR_CLONE"
-    echo "Proses penyalinan BOOT dimulai."
-    cp -a /boot/. "$DIR_CLONE"
-    echo "Proses penyalinan BOOT selesai."
-    umount "$DIR_CLONE"
-    echo "Partisi BOOT dilepas."
-
-    if grep -q "$PART_ROOT" /proc/mounts ; then
+    mount -o rw $PART_BOOT $DIR_CLONE
+    cp -r /boot/* $DIR_CLONE && sync
+    umount $DIR_CLONE
+    if grep -q $PART_ROOT /proc/mounts ; then
         echo "Melepaskan partisi ROOT."
-        umount "$PART_ROOT" || true
+        umount -f $PART_ROOT || true
     fi
-
     echo "Menyalin ROOT..."
-    mount "$PART_ROOT" "$DIR_CLONE"
-    echo "Proses penyalinan ROOT dimulai."
-    cp -a /{bin,etc,home,lib,lib64,opt,root,sbin,selinux,srv,usr,var} "$DIR_CLONE"
-    mkdir -p "$DIR_CLONE"/{dev,media,mnt,proc,run,sys,tmp}
-    echo "Proses penyalinan ROOT selesai."
-    umount "$DIR_CLONE"
-    echo "Partisi ROOT dilepas."
+    mount -o rw $PART_ROOT $DIR_CLONE
+    cp -r /{bin,etc,home,lib,lib64,opt,root,sbin,selinux,srv,usr,var} $DIR_CLONE
+    mkdir -p $DIR_CLONE/{dev,media,mnt,proc,run,sys,tmp}
+    sync
 }
 
 # Skrip utama
 echo -e "\033[32m*****************************************************"
 echo -e "\033[36m       Toolkit clone linux by SUIJUNG\033[0m"
 echo -e "\033[32m*****************************************************\033[0m"
-echo "Daftar disk yang tersedia:"
+echo "Daftar disk yang tersedia: "
 daftar_disk
-
 echo -e "\033[33m"
 read -p "Pilih nomor disk yang akan digunakan: " disk_number
 echo -e "\033[0m"
@@ -98,7 +91,7 @@ fi
 echo -e "\033[33m"
 read -p "Masukkan ukuran mulai partisi BOOT (Contoh: 200M): " start_boot
 read -p "Masukkan ukuran akhir partisi BOOT (Contoh: 250M): " size_boot
-read -p "Masukkan ukuran partisi ROOT (Contoh: 1000M sd 100%): " size_root
+read -p "Masukkan ukuran partisi ROOT (Contoh: 1GB sd 100%): " size_root
 read -p "Masukkan label untuk partisi BOOT (Contoh: BOOT): " label_boot
 read -p "Masukkan label untuk partisi ROOT (Contoh: ROOTFS): " label_root
 echo -e "\033[0m"
@@ -114,34 +107,23 @@ if ! [[ $size_boot =~ ^[0-9]+[KMGT]$ ]]; then
     exit 1
 fi
 
-if ! [[ $size_root =~ ^[0-9]+[KMGT]%?$ ]]; then
+if ! [[ $size_root =~ ^[0-9]+[KMGT]%?$|^100%$ ]]; then
     echo -e "\033[31mError: Ukuran partisi ROOT tidak valid.\033[0m"
     exit 1
 fi
 
-format_partisi "$chosen_disk" "$start_boot" "$size_boot" "$size_root" "$label_boot" "$label_root"
-klon_sistem "$chosen_disk" "$label_boot" "$label_root"
+format_partisi "/dev/$chosen_disk" "$start_boot" "$size_boot" "$size_root" "$label_boot" "$label_root"
+klon_sistem "/dev/$chosen_disk" "$label_boot" "$label_root"
 
+# Update fstab
 echo "Mengupdate fstab..."
-PART_BOOT="/dev/${chosen_disk}1"
-PART_ROOT="/dev/${chosen_disk}2"
-BOOT_UUID=$(blkid -s UUID -o value "$PART_BOOT")
-ROOT_UUID=$(blkid -s UUID -o value "$PART_ROOT")
+BOOT_UUID=$(blkid -s UUID -o value "${PART_BOOT}")
+ROOT_UUID=$(blkid -s UUID -o value "${PART_ROOT}")
+sed -i "s|^UUID=[^ ]* /|UUID=${ROOT_UUID} /|" $DIR_CLONE/etc/fstab
+sed -i "s|^UUID=[^ ]* /boot|UUID=${BOOT_UUID} /boot|" $DIR_CLONE/etc/fstab
+sync
 
-# Tentukan urutan penggantian UUID yang benar
-DIR_CLONE="/ddbr/clone"
-if grep -q "/boot" "$DIR_CLONE/etc/fstab"; then
-    # Entri /boot ada, jadi tukar urutannya
-    sed_order="s|^UUID=[^ ]* /boot|UUID=${BOOT_UUID} /boot|;s|^UUID=[^ ]* /|UUID=${ROOT_UUID} /|"
-else
-    # Entri /boot tidak ada, maka pertahankan urutan aslinya
-    sed_order="s|^UUID=[^ ]* /|UUID=${ROOT_UUID} /|;s|^UUID=[^ ]* /boot|UUID=${BOOT_UUID} /boot|"
-fi
-
-# Jalankan perintah sed dengan urutan yang ditentukan
-sed -i -e "$sed_order" "$DIR_CLONE/etc/fstab"
-
-umount "$DIR_CLONE"
+umount $DIR_CLONE
 
 echo "Menghapus direktori /ddbr..."
 rm -rf /ddbr
