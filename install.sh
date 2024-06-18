bbe1ecddd2aaf7466997159791862ecb27760c36
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-06-18 18:54:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,23 @@
 #!/bin/sh
 
 echo "Start script create MBR and filesystem"
-
 DEV_EMMC=/dev/mmcblk1
 
 echo "Start backup u-boot default"
-
 dd if="${DEV_EMMC}" of=/boot/u-boot-default.img bs=1M count=4
 
 echo "Start create MBR and partittion"
-
 parted -s "${DEV_EMMC}" mklabel msdos
 parted -s "${DEV_EMMC}" mkpart primary fat32 700M 828M
 parted -s "${DEV_EMMC}" mkpart primary ext4 829M 100%
 
 echo "Start restore u-boot"
-
 dd if=/boot/u-boot-default.img of="${DEV_EMMC}" conv=fsync bs=1 count=442
 dd if=/boot/u-boot-default.img of="${DEV_EMMC}" conv=fsync bs=512 skip=1 seek=1
-
 sync
-
 echo "Done"
 
 echo "Start copy system for eMMC."
-
 mkdir -p /ddbr
 chmod 777 /ddbr
 
@@ -72,9 +65,7 @@ e2fsck -n $PART_ROOT
 echo "done."
 
 echo "Copying ROOTFS."
-
 mount -o rw $PART_ROOT $DIR_INSTALL
-
 cd /
 echo "Copy BIN"
 tar -cf - bin | (cd $DIR_INSTALL; tar -xpf -)
@@ -117,7 +108,6 @@ tar -cf - var | (cd $DIR_INSTALL; tar -xpf -)
 sync
 
 echo "Copy fstab"
-
 rm $DIR_INSTALL/etc/fstab
 cp -a /root/install/fstab $DIR_INSTALL/etc/fstab
 
@@ -126,7 +116,6 @@ rm $DIR_INSTALL/usr/bin/ddbr
 
 cd /
 sync
-
 umount $DIR_INSTALL
 
 echo "*******************************************"
