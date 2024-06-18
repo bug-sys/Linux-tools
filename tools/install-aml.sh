9936ae4917bd21fa8619fa5c379b03416e1fd8af
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-06-18 21:48:16 +0700

    Update install-aml.sh

--- tools/install-aml.sh
+++ tools/install-aml.sh
@@ -9,7 +9,7 @@ echo "Start backup u-boot default"
 dd if="${DEV_EMMC}" of=/boot/u-boot-default.img bs=1M count=4
 
 echo "Start Format EMMC"
-mkfs.ext4 -F "/dev/${DEV_EMMC}"
+mkfs.ext4 -F "${DEV_EMMC}"
 
 echo "Start create MBR and partittion"
 
