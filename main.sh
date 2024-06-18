94a32038072b2996a269d19af7e06b1f40508fa2
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-06-18 21:23:59 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,39 +1,26 @@
 #!/bin/bash
 
-# Fungsi untuk menampilkan pesan dialog
-show_dialog() {
-    echo "Silakan pilih skrip yang ingin anda jalankan:"
-    echo "1. clone.sh"
-    echo "2. install-aml.sh"
-    echo -n "Masukkan pilihan Anda (1 atau 2): "
-}
+echo "Silakan pilih skrip yang ingin anda jalankan:"
+echo "1. clone.sh"
+echo "2. install-aml.sh"
+echo -n "Masukkan pilihan Anda (1 atau 2): "
+read choice
 
-# Fungsi untuk menjalankan skrip berdasarkan pilihan
-run_script() {
-    local script_name="$1"
-    echo "Menjalankan skrip $script_name..."
-    chmod +x "/tools/$script_name"  # Memberikan izin eksekusi pada skrip
-    "/tools/$script_name"  # Memanggil skrip dari direktori /tools/
-}
-
-# Meminta pengguna untuk memilih skrip
-while true; do
-    show_dialog
-    read choice
-
-    case "$choice" in
-        1)
-            run_script "clone.sh"
-            break
-            ;;
-        2)
-            run_script "install-aml.sh"
-            break
-            ;;
-        *)
-            echo "Pilihan tidak valid. Silakan masukkan 1 atau 2."
-            ;;
-    esac
-done
+case "$choice" in
+    1)
+        echo "Menjalankan skrip clone.sh..."
+        chmod +x "/tools/clone.sh"  # Memberikan izin eksekusi pada skrip
+        "/tools/clone.sh"  # Memanggil skrip dari direktori /tools/
+        ;;
+    2)
+        echo "Menjalankan skrip install-aml.sh..."
+        chmod +x "/tools/install-aml.sh"  # Memberikan izin eksekusi pada skrip
+        "/tools/install-aml.sh"  # Memanggil skrip dari direktori /tools/
+        ;;
+    *)
+        echo "Pilihan tidak valid. Silakan masukkan 1 atau 2."
+        exit 1
+        ;;
+esac
 
 echo "Selesai."
